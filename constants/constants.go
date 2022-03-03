26f649057d081795df7e64a9cc55c34153943076
zamor1974 <luigi.amoroso@gmail.com>
2022-03-03 17:27:07 +0100

    j

--- constants/constants.go
+++
@@ -1,9 +0,0 @@
-package constants
-
-const DBTYPE = "postgres"
-const DBNAME = "Arduino"
-const DBHOST = "192.168.1.250"
-const DBPORT = 5432
-const DBUSERNAME = "arduinoUser"
-const DBPASSWORD = "arduinoPassword"
-const CURRENTLANG = "it"
